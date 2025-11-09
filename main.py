1dcc9fe66523c2beaaea6388b320ab8e8433a46e
Laura Wangari <karilaura77@gmail.com>
2025-11-10 00:15:12 +0300

    updated dependencies and project routes

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
-# from typing import Union
 from fastapi import FastAPI
-import uvicorn
-# from pydantic import BaseModel
+from app.auth import app as auth_router
+from app.tasks import app as task_router
 # from fastapi import Depends
 
 app = FastAPI()
 
-tasks = []
+# include routers
+app.include_router(auth_router)
+app.include_router(task_router)
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "world!"}
-
-
-@app.post("/tasks/{task_id}")
-def create_task(task: str):
-    tasks.append(task)
-    return tasks
-
-
-@app.get("/tasks/{task_id, task_desc}")
-def get_task(task_id: int, task_desc: str):
-    return task_id, task_desc
-
-
-@app.put("/tasks/{task_id, task_desc}")
-def update_task_desc(task_id: int, task_desc: str):
-    return {"task_desc": task_desc, "task_id": task_id}
+# tasks = []
 
 
 # @app.get("/items/{item_id}")
